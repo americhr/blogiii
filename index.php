8b8ceb94fb8dea82ba6a7962fa877f676506e49c
americhr <68237991+americhr@users.noreply.github.com>
2020-07-13 21:59:12 +0300

    4
    4

---
+++ index.php
@@ -0,0 +1,28 @@
+<meta charset="utf-8">
+<link type="text/css" rel="stylesheet" href="css_style/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="title" content="Order"><meta name="description" content="Order form"><title>Home</title>
+
+	<form action="/submit" method="post" class="x_order_form">
+		<div class="order_form">
+			<div class="block_form">
+				<label class="block_form_label">Full name:</label>
+				<div>
+					<input class="form_input" value="" name="name" type="text" placeholder="Enter a name">
+				</div>
+			</div>
+			<div class="block_form">
+				<label class="block_form_label">Phone:</label>
+				<div>
+					<input class="form_input" value="" name="phone" type="text" placeholder="Enter phone">
+				</div>
+			</div>
+			<div class="clear"></div>
+			<a href="contact.php" type="submit" class="ifr_button">To order</a>
+		</div>
+	</form>
+	<a href="cookie-policy.php">Cookie Policy</a> /
+	<a href="privacy-policy.php">Private Policy</a> /
+	<a href="terms.php">Terms</a>
+
+
